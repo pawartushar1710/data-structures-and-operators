7bf291ff264d9067f26d0662547c7866b294f109
Tushar Pawar <109675413+pawartushar1710@users.noreply.github.com>
2023-07-13 20:00:05 +0530

    Add files via upload

---
+++ string methods/replace.js
@@ -0,0 +1,10 @@
+
+let str = "hello world";
+let newstr = str.replace("world","universe");
+console.log(newstr);
+
+
+// let upper = "tushar";
+
+// let newupper = upper.toLocaleUpperCase("tushar");
+// console.log(newupper);

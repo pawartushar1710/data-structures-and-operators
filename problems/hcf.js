f64ebd885beecb44594a0ec684bcebaa775f9ba5
Tushar Pawar <109675413+pawartushar1710@users.noreply.github.com>
2023-07-18 21:24:42 +0530

    Add files via upload

---
+++ problems/hcf.js
@@ -0,0 +1,15 @@
+
+
+// programme to find HCF of two integers
+
+function findHCF(num1, num2) {
+    let hcf;
+    for (let i = 1; i <= num1 && i <= num2; i++) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            hcf = i;
+        }
+    }
+    return hcf;
+}
+
+console.log(findHCF(12, 18));
